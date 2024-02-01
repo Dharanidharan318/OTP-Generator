89a41819f6590e6210579c2163630d4fd6209393
Dharanidharan <158048616+Dharanidharan318@users.noreply.github.com>
2024-02-01 15:55:16 +0530

    Update otp.js

--- otp.js
+++ otp.js
@@ -2,7 +2,7 @@ function generateOTP() {
     const otpDisplay = document.getElementById('otpDisplay');
     
     // Generate a random 4-digit OTP
-    const otp = Math.floor(100000 + Math.random() * 900000);
+    const otp = Math.floor(1000 + Math.random() * 9000);
 
     // Display the OTP
     otpDisplay.innerText = otp;
